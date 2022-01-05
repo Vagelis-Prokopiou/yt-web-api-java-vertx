79c2cd1645d4d5f3a3bd51c5d1c42de73c93d823
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2022-01-05 20:06:20 +0200

    Getting the json

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -3,30 +3,31 @@ package com.example.starter;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainVerticle extends AbstractVerticle {
 
-  @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      List<User> users = new ArrayList<>();
-      users.add(new User(1, "hello"));
-      req.response()
-//        .putHeader("content-type", "text/plain")
-        .putHeader("content-type", "application/json")
-//        .end("Hello from Vert.x!");
-        .end((Buffer) new JsonArray(users));
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
-  }
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        vertx.createHttpServer().requestHandler(req -> {
+            List<User> users = new ArrayList<>();
+            User user = new User(1, "firstname", "lastname", 25, "vertex");
+            users.add(user);
+            req.response()
+                    .putHeader("content-type", "application/json; charset=utf-8")
+                    .end(Json.encode(users));
+        }).listen(8888, http -> {
+            if (http.succeeded()) {
+                startPromise.complete();
+                System.out.println("HTTP server started on port 8888");
+            } else {
+                startPromise.fail(http.cause());
+            }
+        });
+    }
 }
--- src/main/java/com/example/starter/User.java
+++ src/main/java/com/example/starter/User.java
@@ -1,11 +1,23 @@
 package com.example.starter;
 
 public class User {
-  private int id;
-  private String firstName;
+    public int id;
+    public String firstName;
+    public String lastName;
+    public int age;
+    public String Framework;
 
-  public User(int id, String firstName) {
-    this.id = id;
-    this.firstName = firstName;
-  }
+    public User(
+            int id,
+            String firstName,
+            String lastName,
+            int age,
+            String framework
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        Framework = framework;
+    }
 }
