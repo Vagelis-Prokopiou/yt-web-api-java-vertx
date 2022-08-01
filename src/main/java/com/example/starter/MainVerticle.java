ee40f96aad71ff8621a1791e22156ed7988af780
t_cmuramoto <cleber.muramoto@tinnova.com.br>
2022-08-01 15:39:56 -0300

    feat:Revert to eager allocation using a reified array

--- src/main/java/com/example/starter/MainVerticle.java
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -3,9 +3,6 @@
 
 package com.example.starter;
 
-import java.util.Iterator;
-import java.util.stream.IntStream;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
@@ -28,7 +25,7 @@ public class MainVerticle extends AbstractVerticle {
 //      needs <artifactId>jackson-datatype-jdk8</artifactId> to serialize streams directly
 //		DatabindCodec.mapper().registerModule(new Jdk8Module());
 //		UW = DatabindCodec.mapper().writerFor(new TypeReference<Stream<User>>() {});
-		UW = DatabindCodec.mapper().writerFor(new TypeReference<Iterator<User>>() {
+		UW = DatabindCodec.mapper().writerFor(new TypeReference<User[]>() {
 		});
 		FEATURES = UW.getFactory().getGeneratorFeatures();
 	}
@@ -42,15 +39,18 @@ public class MainVerticle extends AbstractVerticle {
 	final BufferImpl buffer = (BufferImpl) Buffer.buffer(VertxByteBufAllocator.DEFAULT.directBuffer(128 * 1024));
 
 	void handle(HttpServerRequest req) {
-		var users = IntStream.rangeClosed(1, 1001).mapToObj(i -> new User(
-				i,
-				"FirstName" + i,
-				"LastName" + i,
-				25,
-				"Java Vert.x"));
+		var arr = new User[1000];
+		for (var i = 0; i < arr.length; i++) {
+			arr[i] = new User(
+					i,
+					"FirstName" + (i + 1),
+					"LastName" + (i + 1),
+					25,
+					"Java Vert.x");
+		}
 
 		try {
-			UW.writeValue(ByteBufJsonGenerator.allocate(buffer.byteBuf().clear(), FEATURES), users.iterator());
+			UW.writeValue(ByteBufJsonGenerator.allocate(buffer.byteBuf().clear(), FEATURES), arr);
 			req.response().putHeader("content-type", "application/json; charset=utf-8").end(buffer);
 		} catch (Exception e) {
 			req.response().setStatusCode(500).end();
